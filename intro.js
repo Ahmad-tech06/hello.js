f702f119a7021ef22465d127e8d2910974fe8953
Olugbade Ahmad <ahmadolugbade@gmail.com>
2026-03-14 18:05:14 +0100

    Add intro.js file

---
+++ intro.js
@@ -0,0 +1,15 @@
+// intro.js
+
+const name = "Ahmad Olugbade";
+const age =  Twenty; // replace with your real age number
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
+
+console.log('My name is Olugbade Ahmad')
+console.log('I am 20')
+console.log('My favorite programming language is javascript')
+
+    ')
